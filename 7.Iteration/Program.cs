ad95e48624757b54cfe6ab4ed6bbeff8ff9283b5
Ivan Franco <ifranco@tupaca.com>
2019-05-23 19:00:35 -0300

    Iteration exercise - binary gap

---
+++ 7.Iteration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _7.Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+
+            sol.solution(1041);        }
+    }
+}
---
+++ 7.Iteration/Solution.cs
@@ -0,0 +1,10 @@
+namespace _7.Iteration
+{
+    internal class Solution
+    {
+
+        public int solution(int N){
+            return 0;
+        }
+    }
+}
